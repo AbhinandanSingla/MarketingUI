1860e10b3dcdd98b4ad2c196af5148efd4ea7346
Abhinandan Singla <deepusingla0448@gmail.com>
2021-03-21 20:07:53 +0530

    initial

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+let nav_btn = document.querySelector('.nav-btn');
+let nav_menu = document.querySelector('.navbar-menu');
+
+nav_btn.addEventListener('click', () => {
+    nav_menu.classList.toggle('hide')
+})
